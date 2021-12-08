b5a541194deffba4e7ab9a81d7699043841b8fce
rodrigozoran <rodrigozoran@gmail.com>
2021-12-08 00:09:24 -0300

    App de consulta de CEP

---
+++ ConsultaAPI/FrmMain.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace ConsultaAPI
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtComp1 = new System.Windows.Forms.TextBox();
+            this.txtComp2 = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtUndPostagem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CEP:";
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(49, 6);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(242, 20);
+            this.txtCEP.TabIndex = 1;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(58, 244);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(84, 23);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(148, 244);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(87, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ESTADO:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "CIDADE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "ENDEREÇO:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "COMPLEMENTO 2:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "COMPLEMENTO 1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "BAIRRO:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "UNID. POSTAGEM:";
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(120, 55);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(171, 20);
+            this.txtUF.TabIndex = 1;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(120, 79);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(171, 20);
+            this.txtCidade.TabIndex = 1;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(120, 102);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(171, 20);
+            this.txtEndereco.TabIndex = 1;
+            // 
+            // txtComp1
+            // 
+            this.txtComp1.Location = new System.Drawing.Point(120, 128);
+            this.txtComp1.Name = "txtComp1";
+            this.txtComp1.Size = new System.Drawing.Size(171, 20);
+            this.txtComp1.TabIndex = 1;
+            // 
+            // txtComp2
+            // 
+            this.txtComp2.Location = new System.Drawing.Point(120, 151);
+            this.txtComp2.Name = "txtComp2";
+            this.txtComp2.Size = new System.Drawing.Size(171, 20);
+            this.txtComp2.TabIndex = 1;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(120, 175);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(171, 20);
+            this.txtBairro.TabIndex = 1;
+            // 
+            // txtUndPostagem
+            // 
+            this.txtUndPostagem.Location = new System.Drawing.Point(120, 199);
+            this.txtUndPostagem.Name = "txtUndPostagem";
+            this.txtUndPostagem.Size = new System.Drawing.Size(171, 20);
+            this.txtUndPostagem.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 276);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtUndPostagem);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtComp2);
+            this.Controls.Add(this.txtComp1);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Consulta de CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtComp1;
+        private System.Windows.Forms.TextBox txtComp2;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtUndPostagem;
+    }
+}
+
---
+++ ConsultaAPI/FrmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Correios;
+
+namespace ConsultaAPI
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCEP.Text))
+                MessageBox.Show("O campo de CEP está vazio", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+                try
+                {
+                    CorreiosApi correiosApi = new CorreiosApi();
+                    var retorno = correiosApi.consultaCEP(txtCEP.Text);
+
+                    if (retorno is null)
+                    {
+                        MessageBox.Show("CEP NÃO ENCONTRADO", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                    txtUF.Text = retorno.uf;
+                    txtCidade.Text = retorno.cidade;
+                    txtEndereco.Text = retorno.end;
+                    txtComp1.Text = retorno.complemento;
+                    txtComp2.Text = retorno.complemento2;
+                    txtBairro.Text = retorno.bairro;
+                    //txtUndPostagem.Text = retorno.unidadesPostagem;
+                }
+                catch(Exception erro)
+                {
+                    MessageBox.Show("ERRO AO CONSULTAR O CEP: " + erro.Message, "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Sair da Aplicação?", "Saindo...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+    }
+}
